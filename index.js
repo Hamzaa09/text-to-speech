3101e430939cfda5a0facede9a5afc551615ec8e
Muhammad Hamza <hamza036513@gmail.com>
2025-05-07 20:15:49 +0500

    first

---
+++ index.js
@@ -0,0 +1,24 @@
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+let voiceSelect = document.querySelector('select')
+
+document.querySelector("#start").addEventListener("click", () => {
+  speech.text = document.querySelector("#text").value;
+  window.speechSynthesis.speak(speech);
+});
+
+document.querySelector("#stop").addEventListener("click", () => {
+  window.speechSynthesis.cancel()
+});
+
+window.speechSynthesis.onvoiceschanged = () => {
+  voices = window.speechSynthesis.getVoices();
+  voices.forEach((voice, i) => {
+    voiceSelect.options[i] = new Option(voice.name, i)
+  });
+};
+
+
+voiceSelect.addEventListener('change', () => {
+    speech.voice = voices[voiceSelect.value]
+})
